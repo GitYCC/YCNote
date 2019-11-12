8a7447a9b2db9e0dd6a597794f627c883e1cc1dd
ycchen <yc.chen@bridgewell.com>
2019-11-12 11:08:00 +0800

    [feature] Clean up pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,137 +12,41 @@ TIMEZONE = 'Asia/Taipei'
 
 DEFAULT_LANG = u'en'
 
-
-
-# Blogroll
-#LINKS = (('Github', 'https://github.com/GitYCC'),
-#          ('Email', 'ycc.tw.email@gmail.com'),)
-
-# Social widget
-SOCIAL = (('About Me','./about-me.html'),
-          ('Github', 'https://github.com/GitYCC'),
-          ('Email', 'mailto:ycc.tw.email@gmail.com'),)
-
-DEFAULT_PAGINATION = 10
+DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = False
 
-PLUGIN_PATHS = ["plugins"]
+PLUGIN_PATHS = ['plugins']
 
-PLUGINS = ["sitemap","render_math","pelican_alias","related_posts"]
+PLUGINS = ['sitemap', 'render_math', 'pelican_alias', 'related_posts']
 
 ## 配置sitemap 插件
 SITEMAP = {
-    "format": "xml",
-    "priorities": {
-        "articles": 0.7,
-        "indexes": 0.5,
-        "pages": 0.3,
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.7,
+        'indexes': 0.5,
+        'pages': 0.3,
     },
-    "changefreqs": {
-        "articles": "monthly",
-        "indexes": "daily",
-        "pages": "monthly",
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'daily',
+        'pages': 'monthly',
     }
 }
-ATH_JAX = {'align':'center'}
+ATH_JAX = {'align': 'center'}
 
-#FILES_TO_COPY = (
-#    ("extra/robots.txt", "robots.txt"),
-#)
-
-#STATIC_PATHS = [u"img"]
-
-DISQUS_SITENAME = "ycnote-1"
+DISQUS_SITENAME = 'ycnote-1'
 
 
 DEFAULT_DATE_FORMAT = '%Y / %B %d'
 
 RELATED_POSTS_MAX = 10
 
-### Nest ###
-
-# NEST Template
 THEME = 'theme/typerite'
-SITESUBTITLE = u'機器學習(Machine Learning)、深度學習(Deep Learning)、類神經網路(Neural Network)、資料科學(Date Science)、Python、演算法(Algorithm)。'
-# Minified CSS
-NEST_CSS_MINIFY = True
-# Add items to top menu before pages
-MENUITEMS = [('Coding','/category/coding.html'),
-             ('AI.ML','/category/aiml.html'),
-             ('Reading','/category/reading.html'),
-             ('Recording','/category/recording.html'),
-             ('About Me','/about-me.html')]
-# Add header background image from content/images : 'background.jpg'
-NEST_HEADER_IMAGES = 'welcome_front_board.jpg'
-NEST_HEADER_LOGO = '/static/img/favicon.png'
-# Footer
-NEST_SITEMAP_COLUMN_TITLE = u'Sitemap'
-NEST_SITEMAP_MENU = [('Archives', '/archives.html'),('Tags','/tags.html')]
-NEST_SITEMAP_ATOM_LINK = u'Atom Feed'
-NEST_SITEMAP_RSS_LINK = u'RSS Feed'
-NEST_SOCIAL_COLUMN_TITLE = u'Contact Me'
-NEST_LINKS_COLUMN_TITLE = u'Links'
-NEST_COPYRIGHT = u'&copy; YC Note 2018'
-# Footer optional
-NEST_FOOTER_HTML = ''
-# index.html
-NEST_INDEX_HEAD_TITLE = u'YC NOTE'
-NEST_INDEX_HEADER_TITLE = u'YC NOTE'
-NEST_INDEX_HEADER_SUBTITLE = u'一個資料科學家的修煉之路'
-NEST_INDEX_CONTENT_TITLE = u'Last Posts'
-# archives.html
-NEST_ARCHIVES_HEAD_TITLE = u'Archives'
-NEST_ARCHIVES_HEAD_DESCRIPTION = u'Posts Archives'
-NEST_ARCHIVES_HEADER_TITLE = u'Archives'
-NEST_ARCHIVES_HEADER_SUBTITLE = u'Archives for all posts'
-NEST_ARCHIVES_CONTENT_TITLE = u'Archives'
-# article.html
-NEST_ARTICLE_HEADER_BY = u'By'
-NEST_ARTICLE_HEADER_MODIFIED = u'modified'
-NEST_ARTICLE_HEADER_IN = u'in category'
-# author.html
-NEST_AUTHOR_HEAD_TITLE = u'Posts by'
-NEST_AUTHOR_HEAD_DESCRIPTION = u'Posts by'
-NEST_AUTHOR_HEADER_SUBTITLE = u'Posts archives'
-NEST_AUTHOR_CONTENT_TITLE = u'Posts'
-# authors.html
-NEST_AUTHORS_HEAD_TITLE = u'Author list'
-NEST_AUTHORS_HEAD_DESCRIPTION = u'Author list'
-NEST_AUTHORS_HEADER_TITLE = u'Author list'
-NEST_AUTHORS_HEADER_SUBTITLE = u'Archives listed by author'
-# categories.html
-NEST_CATEGORIES_HEAD_TITLE = u'Categories'
-NEST_CATEGORIES_HEAD_DESCRIPTION = u'Archives listed by category'
-NEST_CATEGORIES_HEADER_TITLE = u'文章分類'
-NEST_CATEGORIES_HEADER_SUBTITLE = u'Archives listed by category'
-# category.html
-NEST_CATEGORY_HEAD_TITLE = u'Category Archive'
-NEST_CATEGORY_HEAD_DESCRIPTION = u'Category Archive'
-NEST_CATEGORY_HEADER_TITLE = u'Category'
-NEST_CATEGORY_HEADER_SUBTITLE = u'Category Archive'
-# pagination.html
-NEST_PAGINATION_PREVIOUS = u'Previous'
-NEST_PAGINATION_NEXT = u'Next'
-# period_archives.html
-NEST_PERIOD_ARCHIVES_HEAD_TITLE = u'Archives for'
-NEST_PERIOD_ARCHIVES_HEAD_DESCRIPTION = u'Archives for'
-NEST_PERIOD_ARCHIVES_HEADER_TITLE = u'Archives'
-NEST_PERIOD_ARCHIVES_HEADER_SUBTITLE = u'Archives for'
-NEST_PERIOD_ARCHIVES_CONTENT_TITLE = u'Archives for'
-# tag.html
-NEST_TAG_HEAD_TITLE = u'Tag archives'
-NEST_TAG_HEAD_DESCRIPTION = u'Tag archives'
-NEST_TAG_HEADER_TITLE = u'Tag'
-NEST_TAG_HEADER_SUBTITLE = u'Tag archives'
-# tags.html
-NEST_TAGS_HEAD_TITLE = u'Tags'
-NEST_TAGS_HEAD_DESCRIPTION = u'Tags List'
-NEST_TAGS_HEADER_TITLE = u'Tags'
-NEST_TAGS_HEADER_SUBTITLE = u'Tags List'
-NEST_TAGS_CONTENT_TITLE = u'Tags List'
-NEST_TAGS_CONTENT_LIST = u'tagged'
+SITESUBTITLE = u'YC Note, 機器學習(Machine Learning)、深度學習(Deep Learning)、類神經網路(Neural Network)、資料科學(Date Science)、Python、演算法(Algorithm)'
+
 # Static files
 STATIC_PATHS = ['images','media','static', 'extra/robots.txt', 'extra/favicon.ico', 'extra/logo.svg', 'extra/CNAME']
 EXTRA_PATH_METADATA = {
@@ -153,7 +57,7 @@ EXTRA_PATH_METADATA = {
 
 
 def prev_article(current, articles):
-    articles = sorted(articles, key=lambda x: x.locale_date)
+    articles = sorted(articles, key=lambda x: x.date)
     i = articles.index(current) - 1
     if i >= 0:
         return articles[i]
@@ -162,7 +66,7 @@ def prev_article(current, articles):
 
 
 def next_article(current, articles):
-    articles = sorted(articles, key=lambda x: x.locale_date)
+    articles = sorted(articles, key=lambda x: x.date)
     i = articles.index(current) + 1
     if i < len(articles):
         return articles[i]
@@ -171,3 +75,5 @@ def next_article(current, articles):
 
 
 JINJA_FILTERS = {'prev_article': prev_article, 'next_article': next_article}
+AUTHOR_SAVE_AS = False
+AUTHORS_SAVE_AS = False
